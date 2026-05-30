a980487b18c48b9de355fc7b2724a11a30b91780
omkarakhade <77960829+omkarakhade@users.noreply.github.com>
2026-05-30 15:58:21 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ const nextConfig = {
   images: {
     domains: ['api.qrserver.com'],
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
 }
 module.exports = nextConfig
