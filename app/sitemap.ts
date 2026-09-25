3123345b6b8c30f32d2bec3f2cbabd90fa8aebf3
omkarakhade <77960829+omkarakhade@users.noreply.github.com>
2026-09-25 19:43:22 +0530

    Update base URL in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://samyojak.vercel.app'
+  const baseUrl = 'https://www.samyojak-erp.com'
   const now = new Date()
 
   const staticRoutes = [
